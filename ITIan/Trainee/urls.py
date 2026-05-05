df07878ba5975154eb554be7cd459888b5fde60a
MariamAsall <mariamasal075@gmail.com>
2026-05-05 11:50:46 +0300

    Implement CRUD views, URLs, and templates for Trainee app and style css sheet

--- ITIan/Trainee/urls.py
+++ ITIan/Trainee/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # We will add our CRUD paths here in the next step!
+    path('', views.traineelist, name='traineelist'),
+    path('add/', views.add_trainee, name='add_trainee'),
+    path('update/<int:id>/', views.update_trainee, name='update_trainee'),
+    path('delete/<int:id>/', views.delete_trainee, name='delete_trainee'),
 ]
--- ITIan/Trainee/views.py
+++ ITIan/Trainee/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Trainee
+from Course.models import Course
 
-# Create your views here.
+def traineelist(request):
+    trainees = Trainee.objects.all()
+    return render(request, 'trainee/list.html', {'trainees': trainees})
+
+def add_trainee(request):
+    courses = Course.objects.all()
+    if request.method == 'POST':
+        Trainee.objects.create(
+            first_name=request.POST.get('first_name'),
+            last_name=request.POST.get('last_name'),
+            email=request.POST.get('email'),
+            course_id=request.POST.get('course_id') # Linking the foreign key
+        )
+        return redirect('traineelist')
+    return render(request, 'trainee/add.html', {'courses': courses})
+
+def update_trainee(request, id):
+    trainee = get_object_or_404(Trainee, id=id)
+    courses = Course.objects.all()
+    if request.method == 'POST':
+        trainee.first_name = request.POST.get('first_name')
+        trainee.last_name = request.POST.get('last_name')
+        trainee.email = request.POST.get('email')
+        trainee.course_id = request.POST.get('course_id')
+        trainee.save()
+        return redirect('traineelist')
+    return render(request, 'trainee/update.html', {'trainee': trainee, 'courses': courses})
+
+def delete_trainee(request, id):
+    trainee = get_object_or_404(Trainee, id=id)
+    trainee.delete()
+    return redirect('traineelist')
