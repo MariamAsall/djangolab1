6d8fcb5c09edd7f397f6de2f3a16b4b571134f30
MariamAsall <mariamasal075@gmail.com>
2026-05-05 11:25:48 +0300

    Add fields to Course and Trainee models and apply migrations

--- ITIan/Trainee/models.py
+++ ITIan/Trainee/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from Course.models import Course  # This imports the Course model we just made
 
-# Create your models here.
+class Trainee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    # The ForeignKey links this trainee to a course
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='trainees')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
