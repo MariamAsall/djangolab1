14b7771a8189656a714a12c9f2eee7761261e82b
MariamAsall <mariamasal075@gmail.com>
2026-05-05 11:32:53 +0300

    Setup app-level routing and connect to main urls

---
+++ ITIan/Course/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # We will add our CRUD paths (list, add, update, delete) here in the next step!
+]
---
+++ ITIan/Trainee/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # We will add our CRUD paths here in the next step!
+]
