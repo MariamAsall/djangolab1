b3c3221bed206c9ebd793e97a2fadff8602e0a8e
MariamAsall <mariamasal075@gmail.com>
2026-05-09 23:04:16 +0300

    Update Trainee URLs to match CRUD views

--- ITIan/Trainee/urls.py
+++ ITIan/Trainee/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('update/<int:id>/', views.update_trainee, name='update_trainee'),
     path('delete/<int:id>/', views.delete_trainee, name='delete_trainee'),
 
-    path('accounts/', include('django.contrib.auth.urls')), # Handles login/logout
+    path('accounts/', include('django.contrib.auth.urls')), 
+
+    path('details/<int:id>/', views.trainee_details, name='trainee_details'),
+    path('add/', views.add_trainee, name='add_trainee'),
+    path('delete/<int:id>/', views.delete_trainee, name='delete_trainee'),
 ]
