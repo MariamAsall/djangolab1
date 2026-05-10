3012dba04057b565227fba4900bd05c04194c530
MariamAsall <mariamasal075@gmail.com>
2026-05-11 00:54:23 +0300

    imFIxed forms error in views file

--- ITIan/Trainee/views.py
+++ ITIan/Trainee/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Trainee
 from Course.models import Course
+from .forms import TraineeForm
 
 def traineelist(request):
     trainees = Trainee.objects.filter(is_deleted=False) 
