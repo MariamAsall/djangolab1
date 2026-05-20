2f5aba74fe79b431bdc7b38a85eb6e2109c29af4
MariamAsall <mariamasal075@gmail.com>
2026-05-20 15:24:12 +0300

    fixed errors in views trainee

--- ITIan/Trainee/views.py
+++ ITIan/Trainee/views.py
@@ -4,6 +4,7 @@ from Course.models import Course
 from .forms import TraineeForm
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
