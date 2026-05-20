3e3e34e55bf2de6401c4a8b90c6216c1a0edaeeb
MariamAsall <mariamasal075@gmail.com>
2026-05-20 15:17:18 +0300

    feat: configure global JWT token obtain and refresh endpoints

--- ITIan/Course/urls.py
+++ ITIan/Course/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', views.courselist, name='courselist'),
     path('add/', views.add_course, name='add_course'),
     path('update/<int:id>/', views.update_course, name='update_course'),
     path('delete/<int:id>/', views.delete_course, name='delete_course'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
